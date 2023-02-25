46f23b9de0fa9f2ab3f504e2f1c4e75ec429c8ed
El1nor <ermschense@gmail.com>
2023-02-24 19:51:13 -0600

    Changed name for arm in shoulder command

--- src/main/java/frc/robot/commands/ArmShoulderDriveCommand.java
+++ src/main/java/frc/robot/commands/ArmShoulderDriveCommand.java
@@ -3,16 +3,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Chassis;
 
 /** Default command to drive the Chassis. */
 public class ArmShoulderDriveCommand extends CommandBase {
   private Chassis m_chassis;
+  private Arm m_arm;
   private XboxController xbox;
 
   /** Default command to drive the Chassis. */
-  public ArmShoulderDriveCommand(Chassis c, XboxController x) {
+  public ArmShoulderDriveCommand(Chassis c, Arm a, XboxController x) {
     m_chassis = c;
+    m_arm = a;
     xbox = x;
     addRequirements(m_chassis);
   }
@@ -23,6 +26,7 @@ public class ArmShoulderDriveCommand extends CommandBase {
 
   @Override
   public void execute() {
+
   }
 
   @Override
