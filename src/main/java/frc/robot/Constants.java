919f25d3210abfd92b8a812c06e4b1be9e2d3b10
KieranMusser <59939188+KieranMusser@users.noreply.github.com>
2023-03-28 17:24:43 -0500

    changed can id

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,8 +18,8 @@ public class Constants {
     public static final class DriveConstants {
         public static final int kLeftMotor1Port = 16;
         public static final int kLeftMotor2Port = 18;
-        public static final int kRightMotor1Port = 15;
-        public static final int kRightMotor2Port = 17;
+        public static final int kRightMotor1Port = 17;
+        public static final int kRightMotor2Port = 15;
 
         public static final double kP = 0.02d;//0.011179d;
         public static final double kI = 0;
