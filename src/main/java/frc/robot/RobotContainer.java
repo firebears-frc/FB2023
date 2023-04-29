22b5e44af481ff9c560f72a291c743690ba2e5cf
Noah-M-Reed <115018090+Noah-M-Reed@users.noreply.github.com>
2023-04-28 21:38:10 -0500

    Made a delay before elbow fully stows

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -155,8 +155,11 @@ public class RobotContainer {
 
     // B button = reset position (stow)
     JoystickButton xboxBButton = new JoystickButton(xboxController, XboxController.Button.kB.value);
-    xboxBButton.onTrue((new ArmShoulderSetpointCommand(20, m_arm))
-        .andThen(new ArmElbowSetpointCommand(220, m_arm)));
+    xboxBButton.onTrue((new ArmShoulderSetpointCommand(0, m_arm))
+        .andThen(new ArmElbowSetpointCommand(220, m_arm)
+        .andThen(new WaitCommand(5))
+        .andThen(new ArmElbowSetpointCommand(209, m_arm)
+        )));
 
     // X button = picks up cube and drops cone
     JoystickButton xboxXButton = new JoystickButton(xboxController, XboxController.Button.kX.value);
