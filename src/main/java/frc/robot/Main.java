8f12e948f408b9fcd091b3fc5afd2716af88acb1
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-05-04 16:30:37 -0500

    Change build info to gversion

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,12 +1,7 @@
 package frc.robot;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
@@ -18,33 +13,11 @@ public final class Main {
         RobotBase.startRobot(Robot::new);
     }
 
-    private static String getFileContents(String filename) {
-        // Create the file object
-        File file = new File(Filesystem.getDeployDirectory(), filename);
-        // Open the file stream
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            // Prepare the buffer
-            byte[] data = new byte[(int) file.length()];
-            // Read the data
-            data = inputStream.readAllBytes();
-            // Format into string and return
-            return new String(data, "UTF-8");
-        } catch (IOException e) {
-            // Print exception and return
-            e.printStackTrace();
-            return "Unknown";
-        }
-    }
-
     private static void displayGitInfo() {
-        final NetworkTable table = NetworkTableInstance.getDefault().getTable("Git Info");
-        // Get the branch name and display on the dashboard
-        String branchName = getFileContents("branch.txt");
-        table.getEntry("Branch Name").setValue(branchName);
-
-        // Get the commit hash and display on the dashboard
-        String commitHash = getFileContents("commit.txt");
-        table.getEntry("Commit Hash (Short)").setValue(commitHash.substring(0, 8));
-        table.getEntry("Commit Hash (Full)").setValue(commitHash);
+        final NetworkTable table = NetworkTableInstance.getDefault().getTable("Build Info");
+        table.getEntry("Branch Name").setValue(BuildConstants.GIT_BRANCH);
+        table.getEntry("Commit Hash (Short)").setValue(BuildConstants.GIT_SHA.substring(0, 8));
+        table.getEntry("Commit Hash (Full)").setValue(BuildConstants.GIT_SHA);
+        table.getEntry("Build Time").setValue(BuildConstants.BUILD_DATE);
     }
 }
