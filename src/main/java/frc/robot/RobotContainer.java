ed83d1fffb5fe68168c42e95a0e443d81599cf12
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-06-22 21:42:27 -0500

    Revert "More removing"
    This reverts commit 8b50a02243bc30eb243b9dc22efbccf8ef8b5fc5.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,13 @@
 package frc.robot;
 
+import frc.robot.auto.OneElementWithMobility;
+import frc.robot.auto.OneElementWithMobilityAndEngaged;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Schlucker;
 import frc.robot.subsystems.SchluckerBag;
 import frc.robot.subsystems.SchluckerNeo550;
+import frc.robot.subsystems.Vision;
 import frc.robot.util.GamePiece;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -27,6 +30,8 @@ public class RobotContainer {
     private final Drivetrain drive;
     private final Arm arm;
     private final Schlucker schlucker;
+    private final Vision vision;
+    private final Lights lights;
 
     private final CommandJoystick joystick_1;
     private final CommandJoystick joystick_2;
@@ -42,6 +47,9 @@ public class RobotContainer {
         drive = new Drivetrain();
         arm = new Arm(log);
         schlucker = new SchluckerBag(log); // new SchluckerNeo550();
+        vision = new Vision(chassis::visionPose);
+        lights = new Lights(schlucker::getHeldItem, schlucker::getWantedItem, chassis::isLevel,
+                chassis::isOnChargeStation, chassis::isNotPitching);
 
         joystick_1 = new CommandJoystick(Constants.JOYSTICK_1_PORT);
         joystick_2 = new CommandJoystick(Constants.JOYSTICK_2_PORT);
@@ -49,6 +57,14 @@ public class RobotContainer {
         DriverStation.startDataLog(log);
 
         autoSelector = new SendableChooser<>();
+        autoSelector.setDefaultOption("1 Cone w/ Mobility & Engage",
+                new OneElementWithMobilityAndEngaged(chassis, arm, schlucker, GamePiece.CONE));
+        autoSelector.addOption("1 Cube w/ Mobility & Engage",
+                new OneElementWithMobilityAndEngaged(chassis, arm, schlucker, GamePiece.CUBE));
+        autoSelector.addOption("1 Cone w/ Mobility",
+                new OneElementWithMobility(chassis, arm, schlucker, GamePiece.CONE));
+        autoSelector.addOption("1 Cube w/ Mobility",
+                new OneElementWithMobility(chassis, arm, schlucker, GamePiece.CUBE));
         autoLog = new StringLogEntry(log, "/Auto/Command");
 
         configureButtonBindings();
