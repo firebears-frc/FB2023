39d99a1cf523bb91936420a1de2922b9621a5628
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-10-03 20:31:48 -0500

    Start logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,12 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -26,6 +31,8 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void robotInit() {
+    initializeLogging();
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
@@ -100,4 +107,27 @@ public class Robot extends LoggedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
+  private void initializeLogging() {
+      Logger logger = Logger.getInstance();
+      logger.recordMetadata("Project Name", BuildConstants.MAVEN_NAME);
+      logger.recordMetadata("Branch Name", BuildConstants.GIT_BRANCH);
+      logger.recordMetadata("Commit Hash (Short)", BuildConstants.GIT_SHA.substring(0, 8));
+      logger.recordMetadata("Commit Hash (Full)", BuildConstants.GIT_SHA);
+      logger.recordMetadata("Build Time", BuildConstants.BUILD_DATE);
+
+      if (isReal()) {
+          // Log to USB & Network Tables
+          logger.addDataReceiver(new WPILOGWriter("/media/sda1/"));
+          logger.addDataReceiver(new NT4Publisher());
+      } else {
+          // Replay from log and save to file
+          setUseTiming(false);
+          String logPath = LogFileUtil.findReplayLog();
+          logger.setReplaySource(new WPILOGReader(logPath));
+          logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+      }
+
+      logger.start();
+  }
 }
