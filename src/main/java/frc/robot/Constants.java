ad1f7ad2b1f60fadc02bb11aa330d9326f774a22
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-10-03 20:43:27 -0500

    Increase turning P

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -102,7 +102,7 @@ public final class Constants {
     public static final double kDrivingMinOutput = -1;
     public static final double kDrivingMaxOutput = 1;
 
-    public static final double kTurningP = 1;
+    public static final double kTurningP = 3;
     public static final double kTurningI = 0;
     public static final double kTurningD = 0;
     public static final double kTurningFF = 0;
