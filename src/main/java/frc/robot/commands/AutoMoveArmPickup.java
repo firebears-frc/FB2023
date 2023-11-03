9148794b667d559bc1fc20530f77a486c8ec3330
Arydragons <sunnybgp.icloud.com>
2023-11-02 20:20:25 -0500

    boop

--- src/main/java/frc/robot/commands/AutoMoveArmPickup.java
+++ src/main/java/frc/robot/commands/AutoMoveArmPickup.java
@@ -6,28 +6,52 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Schlucker;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.util.WPIUtilJNI;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.utils.SwerveUtils;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
+
+public class AutoMoveArmPickup extends SequentialCommandGroup {
+  DriveSubsystem m_chassis;
+  Schlucker m_schlucker;
+  Arm m_arm;
 class Multithreading extends Thread {
     public void run () {
         try {
-
-            System.out.println(
-                Thread.currentThread().getId()
-            );
+            (new getDriveCommand(Pose2d , List<Translation2d>, Pose2d ,))
+            + (new ArmElbowSetpointCommand(230, m_arm))
+            + (new ArmShoulderSetpointCommand(MAX_PRIORITY, m_arm)),
+        
+            ;
         }
         catch (Exception e) {
             System.out.println("Failed");
         }
     }
 }
-
-public class AutoMoveArmPickup {
-    public static void Main(String[] args) {
-        int n=8;
-        for (int i = 0; i < n; i++) {
-            Multithreading object = new Multithreading();
-            object.start();
-        }
-    }
 }
+
+
    
