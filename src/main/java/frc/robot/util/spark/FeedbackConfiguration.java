84be1c87a775b67eccc16ae50950ad15b82ed6ce
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2024-01-03 08:17:44 -0600

    Add support for alternative and external encoders

--- src/main/java/frc/robot/util/spark/FeedbackConfiguration.java
+++ src/main/java/frc/robot/util/spark/FeedbackConfiguration.java
@@ -1,7 +1,11 @@
 package frc.robot.util.spark;
 
+import java.util.function.Function;
+
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.MotorFeedbackSensor;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
@@ -41,10 +45,44 @@ public interface FeedbackConfiguration {
 
     public static FeedbackConfiguration builtInEncoder(boolean inverted, double conversionFactor, Integer averageDepth,
             Integer measurementPeriod) {
+        return relativeEncoder(motor -> motor.getEncoder(), inverted, conversionFactor, averageDepth,
+                measurementPeriod);
+    }
+
+    public static FeedbackConfiguration maxAlternativeEncoder(int countsPerRev, boolean inverted,
+            double conversionFactor) {
+        return maxAlternativeEncoder(countsPerRev, inverted, conversionFactor, null, null);
+    }
+
+    public static FeedbackConfiguration maxAlternativeEncoder(int countsPerRev, boolean inverted,
+            double conversionFactor, Integer averageDepth, Integer measurementPeriod) {
+        return relativeEncoder(motor -> {
+            if (!(motor instanceof CANSparkMax))
+                throw new IllegalArgumentException("Alternative encoder mode is only available on a Spark MAX");
+            return ((CANSparkMax) motor).getAlternateEncoder(countsPerRev);
+        }, inverted, conversionFactor, averageDepth, measurementPeriod);
+    }
+
+    public static FeedbackConfiguration flexExternalEncoder(int countsPerRev, boolean inverted,
+            double conversionFactor) {
+        return flexExternalEncoder(countsPerRev, inverted, conversionFactor, null, null);
+    }
+
+    public static FeedbackConfiguration flexExternalEncoder(int countsPerRev, boolean inverted, double conversionFactor,
+            Integer averageDepth, Integer measurementPeriod) {
+        return relativeEncoder(motor -> {
+            if (!(motor instanceof CANSparkFlex))
+                throw new IllegalArgumentException("External encoder mode is only available on a Spark FLEX");
+            return ((CANSparkFlex) motor).getExternalEncoder(countsPerRev);
+        }, inverted, conversionFactor, averageDepth, measurementPeriod);
+    }
+
+    private static FeedbackConfiguration relativeEncoder(Function<CANSparkBase, RelativeEncoder> sensorFunction,
+            boolean inverted, double conversionFactor, Integer averageDepth, Integer measurementPeriod) {
         return new FeedbackConfiguration() {
             @Override
             public MotorFeedbackSensor apply(CANSparkBase motor) {
-                RelativeEncoder encoder = motor.getEncoder();
+                RelativeEncoder encoder = sensorFunction.apply(motor);
                 Util.configureCheckAndVerify(encoder::setInverted, encoder::getInverted, inverted, "inverted");
                 Util.configureCheckAndVerify(encoder::setPositionConversionFactor, encoder::getPositionConversionFactor,
                         conversionFactor, "positionFactor");
